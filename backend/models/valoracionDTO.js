58269e8fcfe061472c53ab5c3dca71c9c622e763
algoroloro <algoroloro@gmail.com>
2020-10-24 19:22:35 +0200

    valoracion dto

---
+++ backend/models/valoracionDTO.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const ValoracionSchema = new mongoose.Schema({
+    tipo: {
+        type: String,
+        required: true
+    },
+    comentario: {
+        type: String,
+        required: true
+    },
+    puntuacion: {
+        type: Number,
+        required: true
+    },
+    terminos: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const ValoracionModel = mongoose.model("Valoracion", ValoracionSchema);
+
+export default ValoracionModel;
