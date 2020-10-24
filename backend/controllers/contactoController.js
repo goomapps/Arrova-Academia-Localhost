383f87e8231e0d612f00e4eaad0c09d4e1cdbc07
algoroloro <algoroloro@gmail.com>
2020-10-24 19:15:23 +0200

    contacto controller

---
+++ backend/controllers/contactoController.js
@@ -0,0 +1,18 @@
+import ContactoModel from "../models/Contacto.js";
+
+const ContactoController = {
+    async insert(req, res) {
+        try {
+            const contacto = await ContactoModel.create(req.body);
+            res.status(201).send(contacto);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({
+                message: "EL CONTACTO NO SE HA PODIDO INSERTAR",
+                error,
+            });
+        }
+    },
+};
+
+export default ContactoController;
