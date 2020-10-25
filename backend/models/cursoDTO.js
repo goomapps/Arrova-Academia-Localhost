9b32957887cc9ba7c61de502cd401090df6d9a48
algoroloro <algoro9697@gmail.com>
2020-10-25 19:36:39 +0100

    curso dto

---
+++ backend/models/cursoDTO.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const CursoSchema = new mongoose.Schema(
+    {
+        nombre: {
+            type: String,
+            required: true,
+        },
+        precio: {
+            type: String,
+            enum: ["mensualidad", "total"],
+            required: true,
+        },
+        cantidad: {
+            type: Number,
+            required: true
+        },
+        adquirido: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+const CursoModel = mongoose.model("Curso", CursoSchema);
+
+export default CursoModel;
