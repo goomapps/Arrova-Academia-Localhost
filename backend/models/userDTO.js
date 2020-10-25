0a9f6942869553b92ed6a65ca4b02a0ea2ec3848
algoroloro <algoro9697@gmail.com>
2020-10-25 20:40:21 +0100

    user dto update

--- backend/models/userDTO.js
+++ backend/models/userDTO.js
@@ -54,6 +54,6 @@ const UserSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const UserModel = mongoose.model("User", UserSchema);
+const UserModel = mongoose.model("Usuario", UserSchema);
 
 export default UserModel;
