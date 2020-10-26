17ed614041ba187692f7a21424fee63c988adf3d
algoroloro <algoro9697@gmail.com>
2020-10-26 19:25:05 +0100

    user dto updated

--- backend/models/userDTO.js
+++ backend/models/userDTO.js
@@ -1,59 +1,21 @@
 import mongoose from "mongoose";
+import Schema from "mongoose";
 
 const UserSchema = new mongoose.Schema(
     {
-        nombre: {
-            type: String,
-            required: true
-        },
-        apellido1: {
-            type: String,
-            required: true,
-        },
-        apellido2: {
-            type: String,
-            required: true
-        },
-        nombreUsuario: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        correo: {
-            type: String,
-            required: true,
-            unique: true,
-        },
-        contrasenya: {
-            type: String,
-            required: true,
-        },
-        fechaNacimiento: {
-            type: String,
-            required: true,
-        },
-        terminos: {
-            type: Boolean,
-            required: true
-        },
-        rol: {
-            type: String,
-            default: "user",
-            enum: ["admin", "user"],
-        },
-        /*
-        cursos: {
-            // TODO: buscar información mapeo cursos por id
-        },
-        tokens: {
-            type: [String],
-            // TODO: buscar información tokens
-        }
-         */
+        nombre: {type: String},
+        apellido1: {type: String},
+        apellido2: {type: String},
+        nombreUsuario: {type: String, unique: true,},
+        correo: {type: String, unique: true,},
+        contrasenya: {type: String},
+        fechaNacimiento: {type: String},
+        terminos: {type: Boolean},
+        rol: {type: String, default: "user", enum: ["admin", "user"]},
+        cursos: {type: Schema.Types.ObjectId, ref: "Curso"},
+        tokens: {type: [String]}
     },
     {timestamps: true}
 );
-
 const UserModel = mongoose.model("Usuario", UserSchema);
-
 export default UserModel;
