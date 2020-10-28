17f6eef2d69bdb57df6b0fa15363509e2e83c063
algoroloro <algoro9697@gmail.com>
2020-10-28 20:20:22 +0100

    modulos iniciales

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from 'src/app/modules/shared/common/header/header.component';
+import {FooterComponent} from 'src/app/modules/shared/common/footer/footer.component';
+import {PageNotFoundComponent} from 'src/app/modules/shared/page-not-found/page-not-found.component';
+import {MainComponent} from 'src/app/modules/shared/layout/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
