6ba2e896bc9ed4fbe525b2e46b9cfee1465fcc2c
ramses797 <rmorenor97@gmail.com>
2020-10-29 13:06:14 +0100

    correo contacto acabado / correo ecn por acabar

--- backend/controllers/ecnController.js
+++ backend/controllers/ecnController.js
@@ -5,6 +5,20 @@ const EcnController = {
         try {
             const ecn = await EcnModel.create(req.body);
             console.log(req.body);
+
+            const mailOptions = {
+                from: 'contacto@arrovacademia.es',
+                to: 'contacto@arrovacademia.es',
+                subject: 'Arrova Academia',
+                html:   `<div style="width: 45em;">
+                            <h3 style="color: black">Corrreo de contacto</h3>
+                            <p style="color: black">Nombre: ${}</p>
+                            <p style="color: black;">Correo: ${}</p>
+                            <p style="color: black; margin-left: 1em">Duda: ${}</p>
+                            <p style="color: black; margin-top: 0.5em">Terminos aceptados: ${}</p>
+                        </div>`
+                }
+            transporter.sendMail(mailOptions);
             // TODO: CONVERTIR EL JSON QUE NOS VIENE DEL BODY A TEXTO PLANO
             // TODO: ENVIAR TEXTO PLANO A NUESTRO EMAIL DE CONTACTO -> CONTACTO@ARROVACADEMIA.ES
             res.status(201).send({
