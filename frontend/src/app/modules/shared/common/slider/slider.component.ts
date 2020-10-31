26ad3ad587298ab3b974694f61d90d30e6590584
algoroloro <algoro9697@gmail.com>
2020-10-31 19:11:26 +0100

    slider start

--- frontend/src/app/modules/shared/common/slider/slider.component.ts
+++ frontend/src/app/modules/shared/common/slider/slider.component.ts
@@ -10,6 +10,20 @@ export class SliderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.showSlides();
   }
 
+  showSlides(): void {
+    let slideIndex = 0;
+    const slides = document.getElementsByClassName('imagenSlider');
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].setAttribute('style', 'display: none;');
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    slides[slideIndex - 1].setAttribute('style', 'position: block;');
+    setTimeout(this.showSlides, 2000);
+  }
 }
