fa07c3dc985a282959513b7dbb1eada0f2efeea2
algoroloro <algoro9697@gmail.com>
2020-11-02 23:10:21 +0100

    ecn valenciano

--- frontend/src/app/modules/estudia-con-nosotros/services/estudia-con-nosotros.service.ts
+++ frontend/src/app/modules/estudia-con-nosotros/services/estudia-con-nosotros.service.ts
@@ -1,9 +1,17 @@
+import {HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import {environment} from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EstudiaConNosotrosService {
+  API_URL: string = environment.API_URL;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  ecnInsert(form: object) {
+    return this.httpClient.post(this.API_URL + '/estudia-con-nosotros/insert', form);
+  }
 }
