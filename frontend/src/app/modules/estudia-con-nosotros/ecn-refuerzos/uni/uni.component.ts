df830ee00e80101491b22006e5cc88e16297702d
algoroloro <algoro9697@gmail.com>
2020-11-18 21:05:06 +0100

    resolviendo error successfull

--- frontend/src/app/modules/estudia-con-nosotros/ecn-refuerzos/uni/uni.component.ts
+++ frontend/src/app/modules/estudia-con-nosotros/ecn-refuerzos/uni/uni.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { NGXLogger } from "ngx-logger";
-import { ToastrService } from "ngx-toastr";
+import { NGXLogger } from 'ngx-logger';
+import { ToastrService } from 'ngx-toastr';
 
-import { EstudiaConNosotrosService } from "../../services/estudia-con-nosotros.service";
-import { EcnRefuerzos } from "../../models/ecn-refuerzos";
+import { EstudiaConNosotrosService } from '../../services/estudia-con-nosotros.service';
+import { EcnRefuerzos } from '../../models/ecn-refuerzos';
 
 @Component({
-  selector: "app-uni",
-  templateUrl: "./uni.component.html",
-  styleUrls: ["./uni.component.scss"],
+  selector: 'app-uni',
+  templateUrl: './uni.component.html',
+  styleUrls: ['./uni.component.scss'],
 })
 export class UniComponent implements OnInit {
   ecnUniversidad;
@@ -21,48 +21,48 @@ export class UniComponent implements OnInit {
     private toastr: ToastrService
   ) {
     this.ecnUniversidad = {
-      comentario: "",
-      correo: "",
+      comentario: '',
+      correo: '',
       terminos: false,
     };
   }
 
   ngOnInit(): void {
-    const cb = document.getElementById("cb");
+    const cb = document.getElementById('cb');
 
-    cb.addEventListener("click", () => {
-      document.getElementById("modal").classList.remove("dnone");
+    cb.addEventListener('click', () => {
+      document.getElementById('modal').classList.remove('dnone');
     });
   }
 
   irParte1(): void {
-    document.getElementById("contenedor2").classList.remove("active");
-    document.getElementById("contenedor1").classList.add("active");
+    document.getElementById('contenedor2').classList.remove('active');
+    document.getElementById('contenedor1').classList.add('active');
   }
   irParte2(): void {
-    document.getElementById("contenedor1").classList.remove("active");
-    document.getElementById("contenedor2").classList.add("active");
+    document.getElementById('contenedor1').classList.remove('active');
+    document.getElementById('contenedor2').classList.add('active');
   }
 
   insert(): void {
     const formulario: EcnRefuerzos = this.ecnUniversidad;
     this.ecnService.ecnInsert(formulario).subscribe({
       next: (data) => {
-        this.logger.log("UNIVERSIDAD INSERTADO CORRECTAMENTE");
-        this.toastr.success("FORMULARIO ENVIADO", "UNIVERSIDAD", {
-          toastClass: "toast success",
+        this.logger.log('UNIVERSIDAD INSERTADO CORRECTAMENTE');
+        this.toastr.success('FORMULARIO ENVIADO', 'UNIVERSIDAD', {
+          toastClass: 'toast success',
         });
         setTimeout(() => {
-          this.router.navigate(["/estudia-con-nosotros/successfull"]);
+          this.router.navigate(['/estudia-con-nosotros/successfull']);
         }, 1000);
       },
       error: (error) => {
-        this.logger.error("FORMULARIO NO SE HA INSERTADO CORRECTAMENTE");
+        this.logger.error('FORMULARIO NO SE HA INSERTADO CORRECTAMENTE');
         this.toastr.error(
-          "EL FORMULARIO NO SE HA RELLENADO CORRECTAMENTE",
-          "HA HABIDO UN ERROR",
+          'EL FORMULARIO NO SE HA RELLENADO CORRECTAMENTE',
+          'HA HABIDO UN ERROR',
           {
-            toastClass: "toast error",
+            toastClass: 'toast error',
           }
         );
       },
