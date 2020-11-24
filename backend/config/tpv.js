da1a1ee5a97b9be9b7918d16bc93eaa727b6b32b
ramses797 <rmorenor97@gmail.com>
2020-11-24 12:49:09 +0100

    pasarela pago

---
+++ backend/config/tpv.js
@@ -0,0 +1,26 @@
+import { SANDBOX_URLS, 
+         PRODUCTION_URLS, 
+         Redsys, 
+         TRANSACTION_TYPES,
+         randomTransactionId,
+         getResponseCodeMessage  } from 'readsys-easy'
+
+const redsys = new Redsys({
+    secretkey: 'sq7HjrUOBfKmC576iLgskD5srU870gj7',
+    URL: SANDBOX_URLS,
+})
+
+const pago = {
+   amount:3500,
+   currency: '978',
+   order: randomTransactionId,
+   merchantName: 'Arrova Academia',
+   merchantCode: '351985874',
+   terminal: '1',
+   transactionType: TRANSACTION_TYPES.AUTHORIZATION,
+   MERCANTURL: '',
+   successURL: '',
+   errorURL: '' 
+}
+
+const form = redsys.redirectPetition(pago);
