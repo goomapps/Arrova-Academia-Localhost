99ff08dfc896a0928d96fe10861bf68c7898b6fc
algoroloro <algoro9697@gmail.com>
2020-11-24 17:19:28 +0100

    secret key

--- backend/config/tpv.js
+++ backend/config/tpv.js
@@ -1,26 +1,28 @@
-import { SANDBOX_URLS, 
-         PRODUCTION_URLS, 
-         Redsys, 
-         TRANSACTION_TYPES,
-         randomTransactionId,
-         getResponseCodeMessage  } from 'readsys-easy'
+import {
+    SANDBOX_URLS,
+    PRODUCTION_URLS,
+    Redsys,
+    TRANSACTION_TYPES,
+    randomTransactionId,
+    getResponseCodeMessage
+} from 'readsys-easy'
 
 const redsys = new Redsys({
-    secretkey: 'sq7HjrUOBfKmC576iLgskD5srU870gj7',
+    secretkey: 'sq7HjrUOBfKmC576ILgskD5srU870gJ7',
     URL: SANDBOX_URLS,
 })
 
 const pago = {
-   amount:3500,
-   currency: '978',
-   order: randomTransactionId,
-   merchantName: 'Arrova Academia',
-   merchantCode: '351985874',
-   terminal: '1',
-   transactionType: TRANSACTION_TYPES.AUTHORIZATION,
-   MERCANTURL: '',
-   successURL: '',
-   errorURL: '' 
+    amount: 3500,
+    currency: '978',
+    order: randomTransactionId,
+    merchantName: 'Arrova Academia',
+    merchantCode: '351985874',
+    terminal: '1',
+    transactionType: TRANSACTION_TYPES.AUTHORIZATION,
+    MERCANTURL: '',
+    successURL: '',
+    errorURL: ''
 }
 
 const form = redsys.redirectPetition(pago);
