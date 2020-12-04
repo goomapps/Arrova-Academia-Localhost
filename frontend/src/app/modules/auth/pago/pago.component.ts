f920f1c2ff2b7e80f2f3e35135454b514e9fed4d
ramses797 <rmorenor97@gmail.com>
2020-12-04 12:08:34 +0100

    pasarela

--- frontend/src/app/modules/auth/pago/pago.component.ts
+++ frontend/src/app/modules/auth/pago/pago.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Stripe } from 'stripe';
 
 @Component({
   selector: 'app-pago',
@@ -9,32 +8,5 @@ import { Stripe } from 'stripe';
 export class PagoComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {
-    /*const stripe = Stripe("pk_test_51HsW6ND0onrjQj4P9FklT1fHyGyplPvNzHzeCKs2nyEZ2NgImtg3J80keFHX5xp5Z8jvTJXob7SJJPjaaxjiU71I00QsKhwQZ7");
-
-        document.getElementById('checkout-button').addEventListener('click', () => {
-            fetch("/create-session", {
-                method: "POST",
-            })
-                .then(function (response) {
-                    console.log(response);
-                    return response.json();
-                })
-                .then(function (session) {
-
-                    return stripe.redirectToCheckout({ sessionId: session.id });
-                })
-                .then(function (result) {
-                    // If redirectToCheckout fails due to a browser or network
-                    // error, you should display the localized error message to your
-                    // customer using error.message.
-                    if (result.error) {
-                        alert(result.error.message);
-                    }
-                })
-                .catch(function (error) {
-                    console.error("Error:", error);
-                });
-        });*/
-  }
+  ngOnInit(): void {}
 }
---
+++ frontend/src/environments/environmentTpv.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  stripe_key:
+    'pk_test_51HsW6ND0onrjQj4P9FklT1fHyGyplPvNzHzeCKs2nyEZ2NgImtg3J80keFHX5xp5Z8jvTJXob7SJJPjaaxjiU71I00QsKhwQZ7',
+};
