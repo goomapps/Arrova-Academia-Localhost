ee5f3ccd0424147da0012ce9fbab6bb52dc655ff
algoroloro <algoro9697@gmail.com>
2020-12-28 22:31:41 +0100

    mas videos

--- frontend/src/app/modules/cursos/cursos-valenciano/intensivo/curso-intensivo/reproductor-intensivo-cieacova/reproductor-intensivo-cieacova.component.ts
+++ frontend/src/app/modules/cursos/cursos-valenciano/intensivo/curso-intensivo/reproductor-intensivo-cieacova/reproductor-intensivo-cieacova.component.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-reproductor-intensivo-cieacova',
-  templateUrl: './reproductor-intensivo-cieacova.component.html',
-  styleUrls: ['./reproductor-intensivo-cieacova.component.scss'],
+  selector: "app-reproductor-intensivo-cieacova",
+  templateUrl: "./reproductor-intensivo-cieacova.component.html",
+  styleUrls: ["./reproductor-intensivo-cieacova.component.scss"],
 })
 export class ReproductorIntensivoCieacovaComponent implements OnInit {
   videoSrc: string[];
@@ -55,6 +55,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
       "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_7_i_8_12_2020_Part2.mp4",
       "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_9_i_10_12_2020_Part1.mp4",
       "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_9_i_10_12_2020_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_15_12_Part1.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_15_2_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_15_12_Part3.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_16_12_Part1.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_16_12_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_16_12_Part3.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_16_12_Part4.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_22_12_Part1.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_22_12_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_22_12_Part3.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_23_12_Part1.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_23_12_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_26_11_Part1.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_26_11_Part2.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_26_11_Part3.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Intensiu_CIEAcova_classe_26_11_Part4.mp4",
     ];
     this.desc = [
       "Morfosintaxis",
@@ -95,6 +111,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
       "CLASE DIA 7/12 PART 2",
       "CLASE DIA 9/12 PART 1",
       "CLASE DIA 9/12 PART 2",
+      "CLASE DIA 15/12 PART 1",
+      "CLASE DIA 15/12 PART 2",
+      "CLASE DIA 15/12 PART 3",
+      "CLASE DIA 16/12 PART 1",
+      "CLASE DIA 16/12 PART 2",
+      "CLASE DIA 16/12 PART 3",
+      "CLASE DIA 16/12 PART 4",
+      "CLASE DIA 22/12 PART 1",
+      "CLASE DIA 22/12 PART 2",
+      "CLASE DIA 22/12 PART 3",
+      "CLASE DIA 23/12 PART 1",
+      "CLASE DIA 23/12 PART 2",
+      "CLASE DIA 26/12 PART 1",
+      "CLASE DIA 26/12 PART 2",
+      "CLASE DIA 26/12 PART 3",
+      "CLASE DIA 26/12 PART 4",
     ];
     this.resumen = [
       "",
@@ -210,6 +242,30 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
         $("#dropfv2").css("display", "none");
       }
     );
+    $("#item55").hover(
+      () => {
+        $("#droppf").show("slow");
+      },
+      () => {
+        $("#droppf").css("display", "none");
+      }
+    );
+    $("#item56").hover(
+      () => {
+        $("#droppre").show("slow");
+      },
+      () => {
+        $("#droppre").css("display", "none");
+      }
+    );
+    $("#item57").hover(
+      () => {
+        $("#droprel").show("slow");
+      },
+      () => {
+        $("#droprel").css("display", "none");
+      }
+    );
     $("#item2").css("display", "none");
     $("#item3").css("display", "none");
     $("#item4").css("display", "none");
@@ -242,6 +298,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
     $("#item36").css("display", "none");
     $("#item37").css("display", "none");
     $("#item38").css("display", "none");
+    $("#item39").css("display", "none");
+    $("#item40").css("display", "none");
+    $("#item41").css("display", "none");
+    $("#item42").css("display", "none");
+    $("#item43").css("display", "none");
+    $("#item44").css("display", "none");
+    $("#item45").css("display", "none");
+    $("#item46").css("display", "none");
+    $("#item47").css("display", "none");
+    $("#item48").css("display", "none");
+    $("#item49").css("display", "none");
+    $("#item50").css("display", "none");
+    $("#item51").css("display", "none");
+    $("#item52").css("display", "none");
+    $("#item53").css("display", "none");
+    $("#item54").css("display", "none");
 
     const unidad1 = document.getElementById("unidad1");
     const unidad2 = document.getElementById("unidad2");
@@ -285,6 +357,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
       $("#item36").css("display", "none");
       $("#item37").css("display", "none");
       $("#item38").css("display", "none");
+      $("#item39").css("display", "none");
+      $("#item40").css("display", "none");
+      $("#item41").css("display", "none");
+      $("#item42").css("display", "none");
+      $("#item43").css("display", "none");
+      $("#item44").css("display", "none");
+      $("#item45").css("display", "none");
+      $("#item46").css("display", "none");
+      $("#item47").css("display", "none");
+      $("#item48").css("display", "none");
+      $("#item49").css("display", "none");
+      $("#item50").css("display", "none");
+      $("#item51").css("display", "none");
+      $("#item52").css("display", "none");
+      $("#item53").css("display", "none");
+      $("#item54").css("display", "none");
     });
 
     unidad2.addEventListener("click", () => {
@@ -326,6 +414,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
       $("#item36").css("display", "block");
       $("#item37").css("display", "block");
       $("#item38").css("display", "block");
+      $("#item39").css("display", "block");
+      $("#item40").css("display", "block");
+      $("#item41").css("display", "block");
+      $("#item42").css("display", "block");
+      $("#item43").css("display", "block");
+      $("#item44").css("display", "block");
+      $("#item45").css("display", "block");
+      $("#item46").css("display", "block");
+      $("#item47").css("display", "block");
+      $("#item48").css("display", "block");
+      $("#item49").css("display", "block");
+      $("#item50").css("display", "block");
+      $("#item51").css("display", "block");
+      $("#item52").css("display", "block");
+      $("#item53").css("display", "block");
+      $("#item54").css("display", "block");
     });
 
     const item1 = document.getElementById("item1");
@@ -366,6 +470,22 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
     const item36 = document.getElementById("item36");
     const item37 = document.getElementById("item37");
     const item38 = document.getElementById("item38");
+    const item39 = document.getElementById("item39");
+    const item40 = document.getElementById("item40");
+    const item41 = document.getElementById("item41");
+    const item42 = document.getElementById("item42");
+    const item43 = document.getElementById("item43");
+    const item44 = document.getElementById("item44");
+    const item45 = document.getElementById("item45");
+    const item46 = document.getElementById("item46");
+    const item47 = document.getElementById("item47");
+    const item48 = document.getElementById("item48");
+    const item49 = document.getElementById("item49");
+    const item50 = document.getElementById("item50");
+    const item51 = document.getElementById("item51");
+    const item52 = document.getElementById("item52");
+    const item53 = document.getElementById("item53");
+    const item54 = document.getElementById("item54");
     const video = document.querySelector("video");
 
     item1.addEventListener("click", () => {
@@ -595,6 +715,102 @@ export class ReproductorIntensivoCieacovaComponent implements OnInit {
       video.src = this.videoSrc[37];
       this.descNum = 37;
       this.resumenNum = 37;
+    });
+    item39.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[38];
+      this.descNum = 38;
+      this.resumenNum = 38;
+    });
+    item40.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[39];
+      this.descNum = 39;
+      this.resumenNum = 39;
+    });
+    item41.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[40];
+      this.descNum = 40;
+      this.resumenNum = 40;
+    });
+    item42.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[41];
+      this.descNum = 41;
+      this.resumenNum = 41;
+    });
+    item43.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[42];
+      this.descNum = 42;
+      this.resumenNum = 42;
+    });
+    item44.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[43];
+      this.descNum = 43;
+      this.resumenNum = 43;
+    });
+    item45.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[44];
+      this.descNum = 44;
+      this.resumenNum = 44;
+    });
+    item46.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[45];
+      this.descNum = 45;
+      this.resumenNum = 45;
+    });
+    item47.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[46];
+      this.descNum = 46;
+      this.resumenNum = 46;
+    });
+    item48.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[47];
+      this.descNum = 47;
+      this.resumenNum = 47;
+    });
+    item49.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[48];
+      this.descNum = 48;
+      this.resumenNum = 48;
+    });
+    item50.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[49];
+      this.descNum = 49;
+      this.resumenNum = 49;
+    });
+    item51.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[50];
+      this.descNum = 50;
+      this.resumenNum = 50;
+    });
+    item52.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[51];
+      this.descNum = 51;
+      this.resumenNum = 51;
+    });
+    item53.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[52];
+      this.descNum = 52;
+      this.resumenNum = 52;
+    });
+    item54.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[53];
+      this.descNum = 53;
+      this.resumenNum = 53;
     });
   }
 
