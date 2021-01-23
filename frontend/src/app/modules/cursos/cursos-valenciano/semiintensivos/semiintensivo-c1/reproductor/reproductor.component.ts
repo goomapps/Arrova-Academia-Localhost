6c0174dbb55f2670f7d7512cd7f13fde6928c2b5
algoroloro <algoro9697@gmail.com>
2021-01-23 19:59:43 +0100

    videos nuevos semiintensivos

--- frontend/src/app/modules/cursos/cursos-valenciano/semiintensivos/semiintensivo-c1/reproductor/reproductor.component.ts
+++ frontend/src/app/modules/cursos/cursos-valenciano/semiintensivos/semiintensivo-c1/reproductor/reproductor.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-reproductor',
-  templateUrl: './reproductor.component.html',
-  styleUrls: ['./reproductor.component.scss'],
+  selector: "app-reproductor",
+  templateUrl: "./reproductor.component.html",
+  styleUrls: ["./reproductor.component.scss"],
 })
 export class ReproductorSemiC1Component implements OnInit {
   videoSrc: string[];
@@ -16,44 +16,86 @@ export class ReproductorSemiC1Component implements OnInit {
   unidad: number[];
 
   constructor() {
-    this.videoSrc = [''];
-    this.desc = [''];
-    this.resumen = [''];
+    this.videoSrc = [
+      "",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Sessio1_1%EF%80%A24.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Sessio1_2%EF%80%A24.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Sessio1_3%EF%80%A24.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C1_Sessio1_4%EF%80%A24.mp4",
+    ];
+    this.desc = [""];
+    this.resumen = [""];
     this.descNum = 0;
     this.resumenNum = 0;
     this.videoSrcNum = 0;
   }
 
   ngOnInit(): void {
-    $('#dropmorfosintaxis').css('display', 'none');
-    $('#item1').hover(
+    $("#dropmorfosintaxis").css("display", "none");
+    $("#item1").hover(
       () => {
-        $('#dropmorfosintaxis').show('slow');
+        $("#dropmorfosintaxis").show("slow");
       },
       () => {
-        $('#dropmorfosintaxis').css('display', 'none');
+        $("#dropmorfosintaxis").css("display", "none");
       }
     );
 
-    const unidad1 = document.getElementById('unidad1');
-    const unidad2 = document.getElementById('unidad2');
+    const unidad1 = document.getElementById("unidad1");
+    const unidad2 = document.getElementById("unidad2");
 
-    unidad1.addEventListener('click', () => {
-      $('#item1').css('display', 'block');
+    unidad1.addEventListener("click", () => {
+      $("#item1").css("display", "block");
+      $("#item2").css("display", "none");
+      $("#item3").css("display", "none");
+      $("#item4").css("display", "none");
+      $("#item5").css("display", "none");
     });
 
-    unidad2.addEventListener('click', () => {
-      $('#item1').css('display', 'none');
+    unidad2.addEventListener("click", () => {
+      $("#item1").css("display", "none");
+      $("#item2").css("display", "block");
+      $("#item3").css("display", "block");
+      $("#item4").css("display", "block");
+      $("#item5").css("display", "block");
     });
 
-    const item1 = document.getElementById('item1');
-    const video = document.querySelector('video');
+    const item1 = document.getElementById("item1");
+    const item2 = document.getElementById("item2");
+    const item3 = document.getElementById("item3");
+    const item4 = document.getElementById("item4");
+    const item5 = document.getElementById("item5");
+    const video = document.querySelector("video");
 
-    item1.addEventListener('click', () => {
+    item1.addEventListener("click", () => {
       video.pause();
       video.src = this.videoSrc[0];
       this.descNum = 0;
       this.resumenNum = 0;
+    });
+    item2.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[1];
+      this.descNum = 1;
+      this.resumenNum = 1;
+    });
+    item3.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[2];
+      this.descNum = 2;
+      this.resumenNum = 2;
+    });
+    item4.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[3];
+      this.descNum = 3;
+      this.resumenNum = 3;
+    });
+    item5.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[4];
+      this.descNum = 4;
+      this.resumenNum = 4;
     });
   }
 
--- frontend/src/app/modules/cursos/cursos-valenciano/semiintensivos/semiintensivo-c2/reproductor/reproductor.component.ts
+++ frontend/src/app/modules/cursos/cursos-valenciano/semiintensivos/semiintensivo-c2/reproductor/reproductor.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import * as $ from 'jquery';
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
-  selector: 'app-reproductor',
-  templateUrl: './reproductor.component.html',
-  styleUrls: ['./reproductor.component.scss'],
+  selector: "app-reproductor",
+  templateUrl: "./reproductor.component.html",
+  styleUrls: ["./reproductor.component.scss"],
 })
 export class ReproductorSemiC2Component implements OnInit {
   videoSrc: string[];
@@ -17,64 +17,94 @@ export class ReproductorSemiC2Component implements OnInit {
 
   constructor() {
     this.videoSrc = [
-      '',
-      'https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/flexio+verbal.mp4',
+      "",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/flexio+verbal.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C2_Sessio1_1%EF%80%A23.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C2_Sessio1_2%EF%80%A23.mp4",
+      "https://arrovacademia.s3.eu-west-2.amazonaws.com/videos/C2_Sessio1_3%EF%80%A23.mp4",
     ];
-    this.desc = ['Determinants', 'Flexió Verbal'];
-    this.resumen = [''];
+    this.desc = ["Determinants", "Flexió Verbal"];
+    this.resumen = [""];
     this.descNum = 0;
     this.resumenNum = 0;
     this.videoSrcNum = 0;
   }
 
   ngOnInit(): void {
-    $('#dropmorfosintaxis').css('display', 'none');
-    $('#dropfv').css('display', 'none');
-    $('#item1').hover(
+    $("#dropmorfosintaxis").css("display", "none");
+    $("#dropfv").css("display", "none");
+    $("#item1").hover(
       () => {
-        $('#dropmorfosintaxis').show('slow');
+        $("#dropmorfosintaxis").show("slow");
       },
       () => {
-        $('#dropmorfosintaxis').css('display', 'none');
+        $("#dropmorfosintaxis").css("display", "none");
       }
     );
-    $('#item2').hover(
+    $("#item2").hover(
       () => {
-        $('#dropfv').show('slow');
+        $("#dropfv").show("slow");
       },
       () => {
-        $('#dropfv').css('display', 'none');
+        $("#dropfv").css("display", "none");
       }
     );
 
-    const unidad1 = document.getElementById('unidad1');
-    const unidad2 = document.getElementById('unidad2');
+    const unidad1 = document.getElementById("unidad1");
+    const unidad2 = document.getElementById("unidad2");
 
-    unidad1.addEventListener('click', () => {
-      $('#item1').css('display', 'block');
-      $('#item2').css('display', 'block');
+    unidad1.addEventListener("click", () => {
+      $("#item1").css("display", "block");
+      $("#item2").css("display", "block");
+      $("#item3").css("display", "none");
+      $("#item4").css("display", "none");
+      $("#item5").css("display", "none");
     });
 
-    unidad2.addEventListener('click', () => {
-      $('#item1').css('display', 'none');
-      $('#item2').css('display', 'none');
+    unidad2.addEventListener("click", () => {
+      $("#item1").css("display", "none");
+      $("#item2").css("display", "none");
+      $("#item3").css("display", "block");
+      $("#item4").css("display", "block");
+      $("#item5").css("display", "block");
     });
 
-    const item1 = document.getElementById('item1');
-    const item2 = document.getElementById('item2');
-    const video = document.querySelector('video');
+    const item1 = document.getElementById("item1");
+    const item2 = document.getElementById("item2");
+    const item3 = document.getElementById("item3");
+    const item4 = document.getElementById("item4");
+    const item5 = document.getElementById("item5");
+    const video = document.querySelector("video");
 
-    item1.addEventListener('click', () => {
+    item1.addEventListener("click", () => {
       video.pause();
       video.src = this.videoSrc[0];
       this.descNum = 0;
       this.resumenNum = 0;
     });
-    item2.addEventListener('click', () => {
+    item2.addEventListener("click", () => {
       video.pause();
       video.src = this.videoSrc[1];
       this.descNum = 1;
       this.resumenNum = 1;
+    });
+    item3.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[2];
+      this.descNum = 2;
+      this.resumenNum = 2;
+    });
+    item4.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[3];
+      this.descNum = 3;
+      this.resumenNum = 3;
+    });
+    item5.addEventListener("click", () => {
+      video.pause();
+      video.src = this.videoSrc[4];
+      this.descNum = 4;
+      this.resumenNum = 4;
     });
   }
 
